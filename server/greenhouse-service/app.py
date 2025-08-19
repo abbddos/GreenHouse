ad693559f320879ee42bf858289115fe4050bf0b
abbddos <abbddos@gmail.com>
2025-08-19 18:51:09 +0300

    correction made

--- server/greenhouse-service/app.py
+++ server/greenhouse-service/app.py
@@ -17,12 +17,17 @@ CORS(app, resources={r"/*": {"origins": Config.CORS_ORIGINS}})
 
 mqtt_client = mqtt.Client(client_id="FlaskAPI") 
 
+
 def on_connect(client, userdata, flags, rc):
     """Callback for when the client receives a CONNACK response from the broker."""
     if rc == 0:
         print("Connected to MQTT Broker!")
     else:
         print(f"Failed to connect, return code {rc}\n")
+        
+mqtt_client.on_connect = on_connect
+mqtt_client.connect("localhost", 1883, 60)
+mqtt_client.loop_start()
 
 from GreenHouse_API import greenhouse_bp
 from Readings_API import readings_bp
@@ -41,16 +46,6 @@ def index():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        
-    try:
-        # Use a public test broker. You should use a local broker or a cloud service for production.
-        broker_address = "localhost"
-        port = 1883
-        mqtt_client.connect(broker_address, port=port, keepalive=60)
-        # Start a background thread to handle MQTT network traffic.
-        mqtt_client.loop_start()
-    except Exception as e:
-        print(f"Error connecting to MQTT broker: {e}")
  
         
     app.run(port = 5002, debug = True)
